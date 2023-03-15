742b88d8db5db1e9c099769b5af24e75108fe71e
John Million <jpmillion@comcast.net>
2023-03-14 20:04:27 -0400

    Setup Entity Framework

---
+++ HotelListingsApi/Data/HotelListingDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListingsApi.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        { 
+
+        }
+    }
+}
