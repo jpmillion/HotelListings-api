ef785ccb11b0a38901e25d821a223a5629a7fb93
John Million <jpmillion@comcast.net>
2023-05-08 08:52:13 -0400

    Add account controller and login dto

---
+++ HotelListingsApi/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using HotelListingsApi.Contracts;
+using HotelListingsApi.Dto.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelListingsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAuthManager _authManager;
+
+        public AccountController(IAuthManager auth) 
+        {
+            _authManager = auth;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> Register([FromBody] ApiUserDto userDto)
+        {
+            IEnumerable<IdentityError> errors = await _authManager.Register(userDto);
+
+            if (errors.Any()) 
+            {
+                foreach (var error in errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> Login([FromBody] LoginDto login)
+        {
+            bool validUser = await _authManager.Login(login);
+
+            if (!validUser)
+            {
+                return Unauthorized();
+            }
+
+            return Ok();
+        }
+    }
+}
